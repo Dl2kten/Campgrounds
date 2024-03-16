dd8bd926ac30e2760f393e5acce48ed624c92559
Dl2kten <dl2kten@gmail.com>
2024-03-16 10:40:51 -0700

    passport set up, user model

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ const methodOverride = require("method-override");
 const ExpressError = require("./utils/ExpressError");
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
 
 const campgroundRoutes = require("./routes/campgrounds");
 const reviewRoutes = require("./routes/reviews");
-
+const User = require("./models/user");
 
 mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp")
     .then(() => {
@@ -44,6 +46,19 @@ const sessionConfig ={
 app.use(session(sessionConfig));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+//test
+app.get("/fakeUser", async(req, res)=>{
+    const user = new User({email: "colttt@gmail.com", username: "colttt"});
+    const newUser = await User.register(user, "chicken");
+    res.send(newUser);
+})
 //flash messages
 app.use((req, res, next) => {
     res.locals.success = req.flash("success");
